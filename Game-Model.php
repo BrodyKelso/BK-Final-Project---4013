b24cb3e685df14a4b305a1180d2ca8d5144fbde8
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-05 16:42:21 -0600

    test

--- Game-Model.php
+++ Game-Model.php
@@ -3,7 +3,7 @@ require_once "util-db.php";
 function selectGames() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Date, Opponent, Location, Result, TeamScore, OpponentScore FROM Games;");
+        $stmt = $conn->prepare("SELECT 'Date', Opponent, 'Location', Result, TeamScore, OpponentScore FROM Games;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
