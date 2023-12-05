6b60fb101217ceb3b2af76980383117ac9c373b2
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-04 23:51:09 -0600

    started on the code

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+    <h1>OU Football 2023 Season Stats</h1>
+<?php
+include "view-footer.php";
+?>
---
+++ util-db.php
@@ -0,0 +1,12 @@
+<?php
+function get_db_connection(){
+   
+    $conn = new mysqli('159.89.47.44', 'brodypke_FinalProjectUser', 'BnY3O@p9Nktd', 'brodypke_Final Project 4013');
+    
+    
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
