4d6d5ff8963e37db57248a4208a0db99c4d8f88f
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-05 19:50:24 -0600

    test

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
    
-    $conn = new mysqli('159.89.47.44', 'brodypke_FinalProjectUser', 'BnY3O@p9Nktd', 'brodypke_Final_Project_4013');
+    $conn = new mysqli('159.89.47.44', 'brodypke_FinalProjectUser', 'BrodyProject', 'brodypke_Final_Project_4013');
     
     
     if ($conn->connect_error) {
