2a27758db21764f02659d6e85d8c5faac140e824
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-05 19:56:28 -0600

    test

--- view-header.php
+++ view-header.php
@@ -1,50 +1,58 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <!-- Link to your local custom Bootstrap CSS file with Vapor theme -->
-    <link href="bootstrap.css" rel="stylesheet">
-  </head>
-  <body>
+    <title><?= $pageTitle ?></title>
+    
+    <!-- Include Bootstrap CSS from a CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
     <div class="container">
-      <!-- Navbar customized for Vapor theme -->
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="Game-View.php">Games</a>
-              </li>
-              <!-- ... other nav items ... -->
-              <li class="nav-item">
-                <a class="nav-link" href="Players.php">Player Roster</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="Players.php">Passing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="Players.php">Rushing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="Players.php">Kicking</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="Players.php">Defense</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- Additional header content here -->
+        <!-- Navbar customized for Vapor theme -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Game-View.php">Games</a>
+                        </li>
+                        <!-- ... other nav items ... -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="Players.php">Player Roster</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Players.php">Passing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Players.php">Rushing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Players.php">Kicking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Players.php">Defense</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Additional header content here -->
     </div>
-  </body>
+    
+    <!-- Include Bootstrap JavaScript dependencies (jQuery, Popper.js, Bootstrap JS) from CDNs -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
