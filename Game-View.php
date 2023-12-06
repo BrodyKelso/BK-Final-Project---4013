5e0ddee9ec0e7c51771afa05726b732246fe1c7f
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-06 14:35:09 -0600

    test

--- Game-View.php
+++ Game-View.php
@@ -3,23 +3,25 @@
     <table>
         <thead>
             <tr>
-                <th>ID</th>
+                <th>GameID</th>
                 <th>Opponent Name</th>
                 <th>Date</th>
                 <th>Location</th>
                 <th>Result</th>
-                <th>Team ID</th>
+                <th>Team Score</th>
+                <th>Opponent Score</th>
             </tr>
         </thead>
         <tbody>
             <?php while ($game = $games->fetch_assoc()) { ?>
                 <tr>
-                    <td><?php echo $game['GameID']; ?></td>
+                    <td><?php echo $game['game_id']; ?></td>
                     <td><?php echo $game['opponent_name']; ?></td>
                     <td><?php echo $game['date']; ?></td>
                     <td><?php echo $game['location']; ?></td>
                     <td><?php echo $game['result']; ?></td>
-                    <td><?php echo $game['team_id']; ?></td>
+                    <td><?php echo $game['team_score']; ?></td>
+                    <td><?php echo $game['opponent_score']; ?></td>
                 </tr>
             <?php } ?>
         </tbody>
