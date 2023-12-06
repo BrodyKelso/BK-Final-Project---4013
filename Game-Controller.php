e9be80b73c8ec9efb18c2c5f912b107ef370d041
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-05 19:43:28 -0600

    test

--- Game-Controller.php
+++ Game-Controller.php
@@ -5,26 +5,9 @@ require_once "Game-Model.php";
 $pageTitle = "Games";
 include "view-header.php";
 
-$games = selectGames(); // Attempt to fetch games
+$games = selectGames(); 
 include "Game-View.php";
-// Check if $games is false, indicating a query error
-if ($games === false) {
-    error_log('selectGames() failed to execute');
-    die('Error fetching games. Please check the error log.');
-}
 
-// Check if $games is null, indicating an unexpected error
-if ($games === null) {
-    error_log('selectGames() returned null');
-    die('Error: selectGames() returned null. Please check the error log.');
-}
-
-// Check if $games has no rows, indicating no games found
-if ($games->num_rows === 0) {
-    error_log('No games returned from the database');
-    die('No games found in the database.');
-}
-    
 
 include "view-footer.php";
 ?>
