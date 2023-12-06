dc364e2dec202f4c23804487c69c2e586a5eb14d
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-05 21:36:08 -0600

    testing

--- Game-View.php
+++ Game-View.php
@@ -1,45 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Games</title>
-    <!-- Link to Bootstrap CSS from a CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <?php include "view-header.php"; // Include the navbar from view-header.php ?>
-
-        <h1>Games</h1>
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Opponent Name</th>
-                        <th>Date</th>
-                        <th>Location</th>
-                        <th>Result</th>
-                        <th>Team ID</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php while ($game = $games->fetch_assoc()) { ?>
-                        <tr>
-                            <td><?php echo $game['opponent_name']; ?></td>
-                            <td><?php echo $game['date']; ?></td>
-                            <td><?php echo $game['location']; ?></td>
-                            <td><?php echo $game['result']; ?></td>
-                            <td><?php echo $game['team_id']; ?></td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <!-- Include Bootstrap JavaScript dependencies (jQuery, Popper.js, Bootstrap JS) from CDNs -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+<h1>Games</h1>
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Opponent Name</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>Result</th>
+                <th>Team ID</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($game = $games->fetch_assoc()) { ?>
+                <tr>
+                    <td><?php echo $game['game_id']; ?></td>
+                    <td><?php echo $game['opponent_name']; ?></td>
+                    <td><?php echo $game['date']; ?></td>
+                    <td><?php echo $game['location']; ?></td>
+                    <td><?php echo $game['result']; ?></td>
+                    <td><?php echo $game['team_id']; ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
