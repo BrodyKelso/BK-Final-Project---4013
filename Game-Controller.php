1f339fabd83e69ad9a52df2651c6777869d34ac4
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-09 14:57:20 -0600

    test

--- Game-Controller.php
+++ Game-Controller.php
@@ -1,43 +1,55 @@
 <?php
-require_once "util-db.php";
-require_once "Game-Model.php";
+require_once("util-db.php");
+require_once("model-games.php");
 
 $pageTitle = "Games";
 include "view-header.php";
 
-$games = selectGames();
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertGame(
+                $_POST['game_id'],
+                $_POST['opponent_name'],
+                $_POST['date'],
+                $_POST['location'],
+                $_POST['result'],
+                $_POST['team_score'],
+                $_POST['opponent_score']
+            )) {
+                echo '<div class="alert alert-success" role="alert">Game added successfully.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Failed to add game.</div>';
+            }
+            break;
 
-// Handle game actions (Add, Edit, Delete)
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $actionType = $_POST["actionType"];
-    
-    if ($actionType === "Add") {
-        // Handle adding a new game
-        $game_id = $_POST["game_id"];
-        $opponent_name = $_POST["opponent_name"];
-        $date = $_POST["date"];
-        $location = $_POST["location"];
-        $result = $_POST["result"];
-        $team_id = $_POST["team_id"];
-        
-        insertGame($game_id, $opponent_name, $date, $location, $result, $team_id);
-    } elseif ($actionType === "Edit") {
-        // Handle editing a game
-        $game_id = $_POST["game_id"];
-        $opponent_name = $_POST["opponent_name"];
-        $date = $_POST["date"];
-        $location = $_POST["location"];
-        $result = $_POST["result"];
-        $team_id = $_POST["team_id"];
-        
-        editGame($game_id, $opponent_name, $date, $location, $result, $team_id);
-    } elseif ($actionType === "Delete") {
-        // Handle deleting a game
-        $game_id = $_POST["game_id"];
-        deleteGame($game_id);
+        case "Edit":
+            if (editGame(
+                $_POST['game_id'],
+                $_POST['opponent_name'],
+                $_POST['date'],
+                $_POST['location'],
+                $_POST['result'],
+                $_POST['team_score'],
+                $_POST['opponent_score']
+            )) {
+                echo '<div class="alert alert-success" role="alert">Game updated successfully.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Failed to update game.</div>';
+            }
+            break;
+
+        case "Delete":
+            if (deleteGame($_POST['game_id'])) {
+                echo '<div class="alert alert-success" role="alert">Game deleted successfully.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Failed to delete game.</div>';
+            }
+            break;
     }
 }
 
-include "Game-View.php";
+$games = selectGames();
+include "view-games.php";
 include "view-footer.php";
 ?>
