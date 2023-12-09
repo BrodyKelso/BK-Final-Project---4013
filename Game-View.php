12b15ca8f8f3e65f9dcad90756f41997d55501db
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-12-09 15:07:47 -0600

    test

--- Game-View.php
+++ Game-View.php
@@ -12,9 +12,9 @@
         <h1>Games</h1>
 
         <!-- Add Button -->
-        <form method="post" action="">
-            <button type="submit" name="actionType" value="Add" class="btn btn-primary">Add Game</button>
-        </form>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addGamesModal">
+            Add Game
+        </button>
 
         <div class="table-responsive">
             <table class="table table-bordered">
@@ -58,9 +58,22 @@
             </table>
         </div>
     </div>
+
     <!-- Include Bootstrap JavaScript dependencies (jQuery, Popper.js, Bootstrap JS) from CDNs -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Add Game Modal -->
+    <div class="modal fade" id="addGamesModal" tabindex="-1" aria-labelledby="addGamesModalLabel" aria-hidden="true">
+        <!-- Add your add game form here -->
+    </div>
+
+    <!-- Edit Game Modals (One for each game) -->
+    <?php while ($game = $games->fetch_assoc()) { ?>
+        <div class="modal fade" id="editGamesModal-<?php echo $game['GameID']; ?>" tabindex="-1" aria-labelledby="editGamesModalLabel" aria-hidden="true">
+            <!-- Add your edit game form for GameID <?php echo $game['GameID']; ?> here -->
+        </div>
+    <?php } ?>
 </body>
 </html>
